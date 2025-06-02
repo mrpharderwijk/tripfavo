4ccb585dfdd8df767e07ac93b9c8c9cc123e3a94
Rocket Science B.V <email@rocketsciencebv.nl>
2025-06-03 00:29:58 +0200

    feat: improve listing-list

--- src/features/listings/components/listings-list/listings-list.tsx
+++ src/features/listings/components/listings-list/listings-list.tsx
@@ -1,9 +1,14 @@
 'use client'
 
 import Image from 'next/image'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { ReactElement } from 'react'
 
+import { FlexBox } from '@/components/atoms/layout/flex-box/flex-box'
+import { FlexBoxItem } from '@/components/atoms/layout/flex-box/flex-box-item/flex-box-item'
+import { Body } from '@/components/atoms/typography/body/body'
+import { Heading } from '@/components/atoms/typography/heading/heading'
 import { PublicListing } from '@/features/listings/types/public-listing'
 
 interface ListingsListProps {
@@ -18,73 +23,90 @@ export function ListingsList({ listings }: ListingsListProps): ReactElement {
   }
 
   return (
-    <section>
+    <section className="w-full">
       {!listings.length && <div>no listings found</div>}
       {listings.map((listing, idx) => (
-        <button
+        <Link
           key={`${listing.id}-${idx}`}
-          className="group cursor-pointer"
-          onClick={() => handleOnClickListing(listing.id)}
+          className="group cursor-pointer w-full"
+          href={`/${listing.id}`}
         >
-          <div className="aspect-square w-80 relative overflow-hidden rounded-xl">
-            <Image
-              src={
-                listing.images.length
-                  ? (listing.images.find((image) => image.isMain)?.url ?? listing.images[0].url)
-                  : ''
-              }
-              alt={''}
-              className="object-cover w-full h-full transition group-hover:scale-110 rounded-md"
-              width={320}
-              height={320}
-            />
-            <div
-              className="absolute top-3 right-3 p-1.5 rounded-full bg-white/40 hover:bg-white/60"
-              aria-label="Add to favorites"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5"
+          <FlexBox flex-direction="col" gap={4}>
+            <div className="w-full relative overflow-hidden rounded-3xl">
+              <Image
+                src={
+                  listing.images.length
+                    ? (listing.images.find((image) => image.isMain)?.url ?? listing.images[0].url)
+                    : ''
+                }
+                alt={''}
+                className="aspect-square object-cover w-full h-full transition group-hover:scale-110"
+                width={1024}
+                height={768}
+              />
+              <div
+                className="absolute top-3 right-3 p-1.5 rounded-full bg-white/40 hover:bg-white/60"
+                aria-label="Add to favorites"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                />
-              </svg>
-            </div>
-          </div>
-          <div className="mt-3 space-y-1">
-            <div className="flex justify-between">
-              <h3 className="font-semibold">Beautiful Villa in Nice</h3>
-              <div className="flex items-center gap-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
                   viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-4 h-4"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
                 >
                   <path
-                    fillRule="evenodd"
-                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                    clipRule="evenodd"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
                   />
                 </svg>
-                <span>4.9</span>
               </div>
             </div>
-            <p className="text-gray-500 text-left">5km from center</p>
-            <p className="text-gray-500 text-left">Oct 15-20</p>
-            <p className="mt-2 text-left">
-              <span className="font-semibold">€120</span>
-              <span className="text-gray-500"> night</span>
-            </p>
-          </div>
-        </button>
+
+            <div className="mt-3 space-y-1">
+              <Heading tag="h3" like="h6" color="primary" font-weight="bold">
+                {listing.title}
+              </Heading>
+
+              <Body size="base-lg" color="secondary" text-align="left" font-weight="semibold">
+                5 km from Monaco
+              </Body>
+
+              <FlexBox flex-direction="row" gap={2} align-items="center">
+                <FlexBoxItem display="flex" flex-direction="row" gap={1}>
+                  <Body size="base-md" color="secondary" text-align="left">
+                    &euro; {listing.priceDetails[0].amount}
+                  </Body>
+                  <Body size="base-md" color="secondary" text-align="left">
+                    per night
+                  </Body>
+                </FlexBoxItem>
+
+                <FlexBoxItem display="flex" flex-direction="row" align-items="center" gap={1}>
+                  <Body size="base-sm" color="secondary" text-align="left">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-4 h-4"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </Body>
+                  <Body size="base-md" color="secondary" text-align="left">
+                    4.9
+                  </Body>
+                </FlexBoxItem>
+              </FlexBox>
+            </div>
+          </FlexBox>
+        </Link>
       ))}
     </section>
   )
