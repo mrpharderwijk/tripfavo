4f5d2e9351f52025bea2b01434a6026a2fb9eb72
Rocket Science B.V <email@rocketsciencebv.nl>
2025-06-05 18:52:16 +0200

    fix: housekeeping

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -9,6 +9,7 @@ import { Footer } from '@/components/molecules/footer/footer'
 import { getPublishedListings } from '@/features/listings/actions/get-listings'
 import { ListingsList } from '@/features/listings/components/listings-list/listings-list'
 import { NavBar } from '@/features/nav-bar/nav-bar'
+
 export default async function Home() {
   const listings = await getPublishedListings()
 
